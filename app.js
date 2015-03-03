17d3b587a2039dfe9d372e3667225dc7f53cacf7
John Peden <johnpeden@Joes-MacBook-Pro.local>
2015-03-03 09:24:22 +0000

    Setup credit card object - Loop for featured cards - Loop for all cards

--- app.js
+++ app.js
@@ -3,165 +3,165 @@ angular.module("creditcardApp", []);
 angular.module("creditcardApp").controller("creditcardCtrl", function($scope) {
 
 	$scope.cards = [
-		amexGold : {
+		{
 			cardName: "American Express Preferred Rewards Gold",
-			cardBonus: "22000",
+			cardBonus: 22000,
 			bonusCurrency: "Membership Rewards",
-			cardFree: "125",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "2000",
-			spendTimeLimit: "90",
+			cardFree: 125,
+			imageName: "amex-gold.jpg", 
+			minSpend: 2000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		amexPlatinum : {
+		{
 			cardName: "American Express Platinum",
-			cardBonus: "35000",
+			cardBonus: 35000,
 			bonusCurrency: "Membership Rewards",
-			cardFree: "450",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "2000",
-			spendTimeLimit: "90",
+			cardFree: 450,
+			imageName: "amex-platinum.jpg", 
+			minSpend: 2000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		amexBritishAirways : {
+		{
 			cardName: "British Airways American Express",
-			cardBonus: "3000",
+			cardBonus: 3000,
 			bonusCurrency: "Avios",
-			cardFree: "0",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "500",
-			spendTimeLimit: "90",
+			cardFree: 0,
+			imageName: "ba.jpg", 
+			minSpend: 500,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		amexBritishAirwaysPremium : {
+		{
 			cardName: "British Airways American Express Premium Plus",
-			cardBonus: "18000",
+			cardBonus: 18000,
 			bonusCurrency: "Avios",
-			cardFree: "125",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "2300",
-			spendTimeLimit: "90",
+			cardFree: 125,
+			imageName: "ba-premium-plus.jpg", 
+			minSpend: 2000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		lloydsAvios : {
+		{
 			cardName: "Lloyds Avios Rewards American Express & MasterCard",
 			cardBonus: "",
 			bonusCurrency: "",
-			cardFree: "24",
-			imageName: "amex-gold.jpeg", 
+			cardFree: 24,
+			imageName: "lloyds-rewards.jpg", 
 			minSpend: "",
 			spendTimeLimit: "",
 			cardURL: "javascript:void(0);"
 		},
-		lloydsAviosPremier : {
+		{
 			cardName: "Lloyds Premier Avios Rewards American Express & MasterCard",
 			cardBonus: "",
 			bonusCurrency: "",
-			cardFree: "140",
-			imageName: "amex-gold.jpeg", 
+			cardFree: 140,
+			imageName: "lloyds-premier.jpg", 
 			minSpend: "",
 			spendTimeLimit: "",
 			cardURL: "javascript:void(0);"
 		},
-		lloydsChoice : {
+		{
 			cardName: "Lloyds Choice Rewards American Express & MasterCard",
 			cardBonus: "",
 			bonusCurrency: "",
-			cardFree: "24",
-			imageName: "amex-gold.jpeg", 
+			cardFree: 24,
+			imageName: "lloyds-choice.jpg", 
 			minSpend: "",
 			spendTimeLimit: "",
 			cardURL: "javascript:void(0);"
 		},
-		tsbAvios : {
+		{
 			cardName: "TSB Avios American Express & MasterCard",
 			cardBonus: "",
 			bonusCurrency: "",
 			cardFree: "",
-			imageName: "amex-gold.jpeg", 
+			imageName: "tsb.jpg", 
 			minSpend: "",
 			spendTimeLimit: "",
 			cardURL: "javascript:void(0);"
 		},
-		tsbAviosPremier : {
+		{
 			cardName: "TSB Premier Avios American Express & MasterCard",
 			cardBonus: "",
 			bonusCurrency: "",
 			cardFree: "",
-			imageName: "amex-gold.jpeg", 
+			imageName: "tsb-premier.jpg", 
 			minSpend: "",
 			spendTimeLimit: "",
 			cardURL: "javascript:void(0);"
 		},
-		virginAtlanticWhite : {
+		{
 			cardName: "Virgin Atlantic White American Express & Visa",
-			cardBonus: "10000",
+			cardBonus: 10000,
 			bonusCurrency: "Virgin Flying Club",
 			cardFree: "",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "2000",
-			spendTimeLimit: "90",
+			imageName: "virgin-white.jpg", 
+			minSpend: 2000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		virginAtlanticBlack : {
+		{
 			cardName: "Virgin Atlantic Black American Express & Visa",
-			cardBonus: "25000",
+			cardBonus: 25000,
 			bonusCurrency: "Virgin Flying Club",
-			cardFree: "14",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "2000",
-			spendTimeLimit: "90",
+			cardFree: 14,
+			imageName: "virgin-black.jpg", 
+			minSpend: 2000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		hiltonHhonors : {
+		{
 			cardName: "Hilton HHonors Platinum Visa",
-			cardBonus: "1",
+			cardBonus: 1,
 			bonusCurrency: "Free stay at Hilton property",
-			cardFree: "125",
-			imageName: "amex-gold.jpeg", 
+			cardFree: 125,
+			imageName: "hilton.jpg", 
 			minSpend: "750",
-			spendTimeLimit: "90",
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		ihgRewards : {
+		{
 			cardName: "IHG Rewards Club Visa",
-			cardBonus: "30000",
+			cardBonus: 30000,
 			bonusCurrency: "IHG Rewards Club",
-			cardFree: "0",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "1",
-			spendTimeLimit: "90",
+			cardFree: 0,
+			imageName: "ihg.jpg", 
+			minSpend: 1,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		ihgRewardsPremium : {
+		{
 			cardName: "IHG Rewards Club Premium Visa",
-			cardBonus: "60000",
+			cardBonus: 60000,
 			bonusCurrency: "IHG Rewards Club",
-			cardFree: "99",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "1",
-			spendTimeLimit: "90",
+			cardFree: 99,
+			imageName: "ihg-premier.jpg", 
+			minSpend: 1,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		},
-		marriottRewards : {
+		{
 			cardName: "Marriott Rewards MasterCard",
-			cardBonus: "10000",
+			cardBonus: 10000,
 			bonusCurrency: "Marriott Rewards",
-			cardFree: "125",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "200",
-			spendTimeLimit: "180",
+			cardFree: 125,
+			imageName: "marriott.jpg", 
+			minSpend: 200,
+			spendTimeLimit: 180,
 			cardURL: "javascript:void(0);"
 		},
-		starwoodAmex : {
+		{
 			cardName: "American Express Preferred Rewards Gold",
-			cardBonus: "10000",
+			cardBonus: 10000,
 			bonusCurrency: "Starwood Preferred Guest",
-			cardFree: "75",
-			imageName: "amex-gold.jpeg", 
-			minSpend: "1000",
-			spendTimeLimit: "90",
+			cardFree: 75,
+			imageName: "spg.jpg", 
+			minSpend: 1000,
+			spendTimeLimit: 90,
 			cardURL: "javascript:void(0);"
 		}
-	]
+	];
 })
