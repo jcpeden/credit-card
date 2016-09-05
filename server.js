293845dba0eee236193ad15dc86131d2d0395928
jcpeden <peden.jc@gmail.com>
2016-09-05 15:15:09 +0100

    Setup public dir

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
 
-    // make sure index is in the right directory. In this case /app/index.html
+    // ejs render automatically looks in the views folder
     res.render('index');
 });
 
