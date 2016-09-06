1f119d83b9f991b2929a80b8f9fa5958f91b19c3
John Peden <john@tweakdigital.co.uk>
2016-09-06 14:39:05 +0100

    Setup gulp-sass for project and improved directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 /* Variables */
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
     bower = require('gulp-bower');
 
 /* Bower */
@@ -11,20 +13,39 @@ gulp.task('bower', function() {
 /* Webserver */
 gulp.task('connect', function() {
     connect.server({
-        root: 'public/',
+        root: 'dist/',
         livereload: true
     });
 });
 
 /* Livereload */
 gulp.task('reload', function () {
-    gulp.src('./public/*.html')
+    gulp.src('./dist/*.html')
         .pipe(connect.reload());
+});
+
+/* Compile SCSS into minified CSS */
+gulp.task('styles', function () {
+    return gulp.src('./src/stylesheet/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths : ['./bower_components/bootstrap-sass/assets/stylesheets']
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+/* Copy */
+gulp.task('copy', function() {
+    gulp.src('./assets/images/**/*')
+        .pipe(gulp.dest('./dist/images'));
 });
 
 /* Watch */
 gulp.task('watch', function () {
-    gulp.watch(['./public/*.html'], ['reload']);
+    gulp.watch('./src/sass/**/*.scss', ['styles']);
+    gulp.watch(['./dist/*.html'], ['reload']);
 });
 
 /* Default */
