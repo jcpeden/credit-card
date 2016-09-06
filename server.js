cfa2d85eb1f932424bcc0dae5d3a2f0a2adfcb44
John Peden <john@tweakdigital.co.uk>
2016-09-06 15:01:10 +0100

    Fixed server.js not loading the app

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ var app = express();
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
-// set the views dir to dist
-app.set('views', __dirname + '/dist');
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/public'));
 
 // set the home page route
 app.get('/', function(req, res) {
@@ -22,5 +19,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
-    console.log('Your app is running on http://localhost:' + port);
+    console.log('Our app is running on http://localhost:' + port);
 });
